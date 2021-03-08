f6a39074d94a46735ded8139ada32b5117ecd329
Erick Murillo <erickmurillo22@gmail.com>
2021-03-08 08:55:39 -0600

    ---

--- movies_app/settings.py
+++ movies_app/settings.py
@@ -131,3 +131,9 @@ CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
